52bb81673eb11c747485e2133a8afb78d04e2002
M Ridwan Taufik Hidayat <mr.taufikhidayat.1995@gmail.com>
2020-01-06 17:54:20 +0700

    add api leave room

--- src/main/java/com/daksa/vicall/controller/ParticipantSessionController.java
+++ src/main/java/com/daksa/vicall/controller/ParticipantSessionController.java
@@ -4,20 +4,23 @@ import com.daksa.vicall.domain.Participant;
 import com.daksa.vicall.domain.ParticipantSession;
 import com.daksa.vicall.exception.AuthenticationException;
 import com.daksa.vicall.exception.ParticipantNotExistException;
+import com.daksa.vicall.model.LeaveRoom;
 import com.daksa.vicall.model.RoomToken;
 import com.daksa.vicall.repository.ParticipantRepository;
 import com.daksa.vicall.repository.ParticipantSessionRepository;
 import com.daksa.vicall.service.RoomService;
+import io.openvidu.java.client.OpenVidu;
 import io.openvidu.java.client.OpenViduHttpException;
 import io.openvidu.java.client.OpenViduJavaClientException;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 @RequestScoped
-@Path("user/session")
+@Path("room")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class ParticipantSessionController {
@@ -31,7 +34,7 @@ public class ParticipantSessionController {
 
 	@GET
 	@Path("join")
-	public RoomToken getToken(@QueryParam("room") String room, @QueryParam("sessionId") String sessionId)
+	public RoomToken join(@QueryParam("room") String room, @QueryParam("sessionId") String sessionId)
 			throws AuthenticationException, ParticipantNotExistException, OpenViduJavaClientException, OpenViduHttpException {
 		ParticipantSession session = participantSessionRepository.find(sessionId);
 		if (session == null) {
@@ -42,5 +45,15 @@ public class ParticipantSessionController {
 			throw new ParticipantNotExistException();
 		}
 		return roomService.joinRoom(room, participant);
+	}
+
+	@POST
+	@Path("leave")
+	public void removeUser(LeaveRoom param) throws AuthenticationException {
+		ParticipantSession session = participantSessionRepository.find(param.getSessionId());
+		if (session == null) {
+			throw new AuthenticationException();
+		}
+		roomService.leaveRoom(param);
 	}
 }
--- src/main/java/com/daksa/vicall/service/RoomService.java
+++ src/main/java/com/daksa/vicall/service/RoomService.java
@@ -1,8 +1,11 @@
 package com.daksa.vicall.service;
 
 import com.daksa.vicall.domain.Participant;
+import com.daksa.vicall.model.LeaveRoom;
 import com.daksa.vicall.model.RoomToken;
 import io.openvidu.java.client.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
@@ -12,12 +15,13 @@ import java.util.concurrent.ConcurrentHashMap;
 
 @ApplicationScoped
 public class RoomService implements Serializable {
+	private static final Logger LOG = LoggerFactory.getLogger(RoomService.class);
 	private static final long serialVersionUID = 1L;
 	private Map<String, Session> roomMap = new ConcurrentHashMap<>();
 	private Map<String, Map<String, OpenViduRole>> roomNameTokenMap = new ConcurrentHashMap<>();
 	private OpenVidu openVidu;
-	private static final String URL = "https://localhost:4443";
-	private static final String SECRET = "MY_SECRET";
+	private static final String URL = "https://openvidu.daksa.co.id:4443/";
+	private static final String SECRET = "daksadaksa";
 
 	@PostConstruct
 	public void init() {
@@ -33,16 +37,35 @@ public class RoomService implements Serializable {
 				.role(role)
 				.build();
 		if (roomMap.get(room) != null) {
+			LOG.info("Room {} already exist", room);
 			String token = roomMap.get(room).generateToken(tokenOptions);
 			roomNameTokenMap.get(room).put(token, role);
 			return new RoomToken(token);
 		} else {
+			LOG.info("Creating room {}", room);
 			Session session = openVidu.createSession();
 			String token = session.generateToken(tokenOptions);
 			this.roomMap.put(room, session);
 			this.roomNameTokenMap.put(room, new ConcurrentHashMap<>());
 			this.roomNameTokenMap.get(room).put(token, role);
 			return new RoomToken(token);
+		}
+	}
+
+	public void leaveRoom(LeaveRoom param) {
+		String roomId = param.getRoomId();
+		if (this.roomMap.get(roomId) != null && this.roomNameTokenMap.get(roomId) != null) {
+
+			if (this.roomNameTokenMap.get(roomId).remove(param.getToken()) != null) {
+				if (this.roomNameTokenMap.get(roomId).isEmpty()) {
+					this.roomMap.remove(roomId);
+				}
+			} else {
+				LOG.info("Problems in the app server: the TOKEN wasn't valid");
+			}
+
+		} else {
+			LOG.info("Problems in the app server: the SESSION does not exist");
 		}
 	}
 }
